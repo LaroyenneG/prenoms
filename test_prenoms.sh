02e7f23ddd88124125965a96738023f3e4103aac
LaroyenneG <laroyenneg@gmail.com>
2017-12-01 10:58:24 +0100

    correction bugs du filtre des genres

--- test_prenoms.sh
+++ test_prenoms.sh
@@ -1,12 +1,12 @@
-prenoms/prenoms.bsh -i 
-prenoms/prenoms.bsh -c 
-prenoms/prenoms.bsh -a 1 A boy - - 
-prenoms/prenoms.bsh -a mamanpourlavie a girl no no 
-prenoms/prenoms.bsh -a bebepassion A garcon - no 
-prenoms/prenoms.bsh -d -e
-prenoms/prenoms.bsh -a 4 a fille 4 5 -a 4 a garcon 8 - 
-prenoms/prenoms.bsh -a quelprenom r boy - 5 
-prenoms/prenoms.bsh -a gic b fille 12 - 
-prenoms/prenoms.bsh -a nai c garcon 3 - -a nai d boy - 2 
-prenoms/prenoms.bsh -d -e -m result.txt
+./prenoms.bsh -i 
+./prenoms.bsh -c 
+./prenoms.bsh -a 1 A boy - - 
+./prenoms.bsh -a mamanpourlavie a girl no no 
+./prenoms.bsh -a bebepassion A garcon - no 
+./prenoms.bsh -d -e
+./prenoms.bsh -a 4 a fille 4 5 -a 4 a garcon 8 - 
+./prenoms.bsh -a quelprenom r boy - 5 
+./prenoms.bsh -a gic b fille 12 - 
+./prenoms.bsh -a nai c garcon 3 - -a nai d boy - 2 
+./prenoms.bsh -d -e -m result.txt
 
